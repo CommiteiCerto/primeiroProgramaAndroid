5398b69c0686f3c762f7139be01d696a0e579c23
CommiteiCerto <sgl-ti2019>
2020-01-09 16:03:46 -0300

    Primeiro programa em android

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+
+        TextView texto = findViewById(R.id.cliqueAbaixo);
+        texto.setText("Helo zamim m");
+
+    }
+
+}
